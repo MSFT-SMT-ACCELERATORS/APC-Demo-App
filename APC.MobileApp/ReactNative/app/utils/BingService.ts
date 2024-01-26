46ad6a51fb7d6a5aa85b4158979d73032d58b976
Javier Giner <javier.giner@turingchallenge.com>
2024-01-26 14:50:43 +0100

    feat: Bing Service to translate coords to location

---
+++ APC.MobileApp/ReactNative/app/utils/BingService.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { LocationObjectCoords } from "expo-location";
+
+export const translateCoordsToLocation = async (coords: LocationObjectCoords, verbosePlaceNames: boolean = false) => {
+    const apiKey = 'ApOlwQ1nNnrqWQ2jtz9Wyr27aAsnYsJpDeSr35Ej_-0_ntLEh0Ro0ZBXil0Rlnec';
+    const res = await axios.get(`http://dev.virtualearth.net/REST/v1/Locations/${coords.latitude},${coords.longitude}?verboseplacenames=${verbosePlaceNames}&key=${apiKey}`);
+
+    if(res.status == 200 && res.data && res.data.resourceSets[0].resources[0].address) {
+        const address = res.data.resourceSets[0].resources[0].address;
+        
+        const country = address.countryRegion;
+        const state = address.adminDistrict
+        const city = address.locality;
+
+        return {
+            country: address.countryRegion,
+            state: address.adminDistrict,
+            city: address.locality
+        }
+    }
+
+    return null;
+}
