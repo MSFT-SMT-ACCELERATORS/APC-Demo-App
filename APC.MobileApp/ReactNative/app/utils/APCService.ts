e09ea78e341df6978014ba751b30eedfefca0d47
JavierClaroTC <javier.claro@turingchallenge.com>
2024-02-19 09:57:44 +0100

    API v2

--- APC.MobileApp/ReactNative/app/utils/APCService.ts
+++ APC.MobileApp/ReactNative/app/utils/APCService.ts
@@ -4,7 +4,7 @@ import { Platform } from 'react-native';
 import axios from 'axios'
 import { storeConfigurations, readConfigurations, updateConfiguration, AppConfiguration, ConnectionMode } from './SettingsService'
 import { useApiClient } from '../api/ApiClientProvider';
-import { APCApi } from '../api/generated';
+import { APCApi, AuthApi, Configuration } from '../api/generated';
 import * as BingService from '../utils/BingService'
 import { LocationObjectCoords } from 'expo-location';
 
@@ -56,40 +56,40 @@ export const matchesAPCLocation = async (client: APCApi, coords: Location.Locati
     return {
         verificationResult: await matchesCoords(coords, apcLocation.coords)
     }
-    
 
-    const config = await readConfigurations();
 
-    if (config.connectionMode == ConnectionMode.Mock || config.connectionMode == ConnectionMode.Offline) {
-        const mockAPCLocation = await getAPCLocation(client);
+    // const config = await readConfigurations();
 
-        return {
-            verificationResult: await matchesCoords(coords, mockAPCLocation.coords)
-        }
-    }
+    // if (config.connectionMode == ConnectionMode.Mock || config.connectionMode == ConnectionMode.Offline) {
+    //     const mockAPCLocation = await getAPCLocation(client);
 
-    const ip = await ipify();
+    //     return {
+    //         verificationResult: await matchesCoords(coords, mockAPCLocation.coords)
+    //     }
+    // }
 
-    const response = await client.aPCVerifyDeviceLocationPost({
-        deviceId: {
-            ipv4Address: ip,
-            ipv6Address: "ipv6Address",
-            networkAccessIdentifier: "networkAccessIdentifier",
-            phoneNumber: "phoneNumber"
-        },
-        locationArea: {
-            areaType: "CIRCLE",
-            circle: {
-                latitude: coords.latitude,
-                longitude: coords.longitude,
-                radius: coords.accuracy ?? undefined
-            }
-        },
-        networkId: "networkId"
-    });
+    // const ip = await ipify();
 
-    console.log(response.data);
-    return response.data;
+    // const response = await client.aPCVerifyDeviceLocationPost({
+    //     deviceId: {
+    //         ipv4Address: ip,
+    //         ipv6Address: "ipv6Address",
+    //         networkAccessIdentifier: "networkAccessIdentifier",
+    //         phoneNumber: "phoneNumber"
+    //     },
+    //     locationArea: {
+    //         areaType: "CIRCLE",
+    //         circle: {
+    //             latitude: coords.latitude,
+    //             longitude: coords.longitude,
+    //             radius: coords.accuracy ?? undefined
+    //         }
+    //     },
+    //     networkId: "networkId"
+    // });
+
+    // console.log(response.data);
+    // return response.data;
 }
 
 export const getPhoneNumber = async (apiClient: APCApi): Promise<string> => {
@@ -98,12 +98,43 @@ export const getPhoneNumber = async (apiClient: APCApi): Promise<string> => {
     if (config.connectionMode == ConnectionMode.Mock || config.connectionMode == ConnectionMode.Offline) {
         return config.offlinePhoneNumber;
     }
+    // if (config.connectionMode == ConnectionMode.Offline) {
+    //     return config.offlinePhoneNumber;
+    // }
+    // const mockHeader = config.connectionMode == ConnectionMode.Mock ? { headers: { 'X-Use-Mock': true } } : undefined;
 
     const publicIp = await ipify();
 
-    const response = await apiClient.aPCPhoneNumberGet("networkId", "deviceIdPhoneNumber", "deviceIdNetworkAccessIdentifier", publicIp, "deviceIdIpv6Address");
+    const response = await apiClient.apiAPCNumberVerificationNumberretrievePost(
+        {
+            identifierType: 'IPv4',
+            identifier: publicIp
+        });
 
-    return response.data.devicePhoneNumber ?? "";
+    return response.data.phoneNumber ?? "";
+}
+
+
+export const getNetworkCode = async (apiClient: APCApi): Promise<string> => {
+    const config = await readConfigurations();
+
+    if (config.connectionMode == ConnectionMode.Mock || config.connectionMode == ConnectionMode.Offline) {
+        return config.offlinePhoneNumber;
+    }
+    // if (config.connectionMode == ConnectionMode.Offline) {
+    //     return config.offlinePhoneNumber;
+    // }
+    // const mockHeader = config.connectionMode == ConnectionMode.Mock ? { headers: { 'X-Use-Mock': true } } : undefined;
+
+    const publicIp = await ipify();
+
+    const response = await apiClient.apiAPCDeviceNetworkNetworkretrievePost(
+        {
+            identifierType: 'IPv4',
+            identifier: publicIp
+        });
+
+    return response.data.networkCode ?? "";
 }
 
 export const checkSimChange = async (apiClient: APCApi) => {
@@ -113,12 +144,19 @@ export const checkSimChange = async (apiClient: APCApi) => {
         return config.offlineLastSimChange;
     }
 
-    const phoneNumber = await getPhoneNumber(apiClient);
-
+    const phoneNumber = await getPhoneNumberAPC();
+    const networkCode = await getNetworkCode(apiClient);
     const mockHeader = config.connectionMode == ConnectionMode.Mock ? { headers: { 'X-Use-Mock': true } } : undefined;
-    const response = await apiClient.aPCCheckSwapPost({ network: 'network', phoneNumber: { number: phoneNumber } }, mockHeader)
+    const response = await apiClient.apiAPCSimSwapSimSwapverifyPost({
+        phoneNumber: phoneNumber,
+        maxAgeHours: 0,
+        networkIdentifier: {
+            identifierType: networkCode,
+            identifier: ''
+        }
+    }, mockHeader)
 
-    return response.data.swapped;
+    return response.data.verificationResult;
 }
 
 
@@ -159,22 +197,22 @@ export const getDeviceGPSLocation = async () => {
 //         }
 //         console.log("Requesting gps permission... OK");
 //     }
- 
+
 //     const config = await readConfigurations();
 //     await getLocationPermission();
- 
+
 //     let location: Location.LocationObject;
 //     console.log("Getting current position...");
- 
+
 //     location = await Location.getCurrentPositionAsync({ accuracy: Location.Accuracy.Balanced });
 //     let coords = location.coords;
 //     coords = getLocationCoords(40.4168, -3.7038, 0);
 //     console.log("Getting current position... OK");
- 
+
 //     const bingLocation = config.connectionMode != ConnectionMode.Offline ? await BingService.translateCoordsToLocation(coords) : undefined;
- 
+
 //     return { coords: coords, location: bingLocation } as Position;
- 
+
 // }
 
 export const getLocationCoords = (latitude: number, longitude: number, accuracy: number = 1000) => {
@@ -236,4 +274,51 @@ export const ipify = async () => {
         console.error("Error al obtener la dirección IP en la web", error);
         return "-";
     }
+}
+
+
+export const getToken = async (): Promise<string> => {
+
+    const configuration = new Configuration({ basePath: "https://apc-proxy-prv-002.azurewebsites.net" });
+    const authApiClient = new AuthApi(configuration);
+    // const config = await readConfigurations();
+    // const mockHeader = config.connectionMode == ConnectionMode.Mock ? { headers: { 'X-Use-Mock': true } } : undefined;
+    const response = await authApiClient.apiAuthTokenGet();
+
+    if (response.data.token === null || response.data.token === undefined) {
+        throw new Error('Token is missing from the response');
+    }
+
+    return response.data.token;
+}
+
+
+export const getPhoneNumberAPC = async () => {
+
+    const token= await getToken();
+    const config = {
+        headers: {
+          'Authorization':token,
+          'Content-Type': 'application/json'
+        }
+      };
+    
+      const publicIp = await ipify();
+      const data = {
+        identifierType: 'IPv4',
+        identifier: publicIp
+      };
+    
+      try {
+        const response = await axios.post('https://eastus.prod.apcgatewayapi.azure.com/number-verification/number:retrieve',
+        data,
+        config);
+
+        console.log(response.data); 
+    
+        return response.data;
+      } catch (error) {
+        console.error('Error en la solicitud POST');
+        throw error; 
+      }
 }
