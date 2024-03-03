2bfcf2eb630b2d83784a0c6f7db101d596ffb88d
Javier Giner <javier.giner@turingchallenge.com>
2024-03-04 00:03:45 +0100

    bugfix: Bing API call failing on Android release

--- APC.MobileApp/ReactNative/app/utils/BingService.ts
+++ APC.MobileApp/ReactNative/app/utils/BingService.ts
@@ -9,7 +9,7 @@ export interface Location {
 
 export const translateCoordsToLocation = async (coords: LocationObjectCoords, verbosePlaceNames: boolean = false) => {
     const apiKey = 'ApOlwQ1nNnrqWQ2jtz9Wyr27aAsnYsJpDeSr35Ej_-0_ntLEh0Ro0ZBXil0Rlnec';
-    const res = await axios.get(`http://dev.virtualearth.net/REST/v1/Locations/${coords.latitude},${coords.longitude}?verboseplacenames=${verbosePlaceNames}&key=${apiKey}`);
+    const res = await axios.get(`https://dev.virtualearth.net/REST/v1/Locations/${coords.latitude},${coords.longitude}?verboseplacenames=${verbosePlaceNames}&key=${apiKey}`);
 
     if (res.status == 200 && res.data && res.data.resourceSets[0].resources[0].address) {
         const address = res.data.resourceSets[0].resources[0].address;
